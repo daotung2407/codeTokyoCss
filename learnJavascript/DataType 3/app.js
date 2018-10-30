56796d5b8521d5624c7286dc8f43ed805d5a514c
tung <robinsaurang@gmail.com>
2018-10-30 21:36:43 +0700

    Datatype (Array)

--- learnJavascript/DataType 3/app.js
+++ learnJavascript/DataType 3/app.js
@@ -1,16 +1,17 @@
 /**
- * Sử dụng kiến thức đã học khai báo một vài object khác nhau mà bạn có thể tưởng tượng ra. Ví dụ:
- *  var myCommunity = {
- *    name: 'Coders.Tokyo',
- *    numberOfMembers: 10000,
- *    isAwesome: true
- *  };
+ * Cách sử dụng Array và Object khác nhau như thế nào. Viết câu trả lời ở phía dưới.
  */
-var mySister = {
-    name: 'Thao',
-    height:165,
-    age: 26,
-};
-mySister['height'] = 170;
 
-console.log(mySister);
+
+
+
+// Trả lời
+// object thường để mô tả 1 đối tượng trong đó sẽ có các thuộc tính của đối tượng đó
+// Array thường được sử dụng để tạo list danh sách các object
+
+var ListsEmployee = [
+    {id: 001, name: 'Nguyễn Mạnh Hùng', phone: 0123456789},
+    {id: 002, name: 'Lê Tuấn Anh', phone: 0954789765},
+    {id: 003, name: 'Lê Hoàng Anh', phone: 0989897575},
+]
+console.log(ListsEmployee)
