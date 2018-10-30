90b74c78bf383af07548d9c0fd0eac0fdbe1ba54
tung <robinsaurang@gmail.com>
2018-10-30 20:52:20 +0700

    Datatype (object)

---
+++ learnJavascript/DataType 3/app.js
@@ -0,0 +1,16 @@
+/**
+ * Sử dụng kiến thức đã học khai báo một vài object khác nhau mà bạn có thể tưởng tượng ra. Ví dụ:
+ *  var myCommunity = {
+ *    name: 'Coders.Tokyo',
+ *    numberOfMembers: 10000,
+ *    isAwesome: true
+ *  };
+ */
+var mySister = {
+    name: 'Thao',
+    height:165,
+    age: 26,
+};
+mySister['height'] = 170;
+
+console.log(mySister);
